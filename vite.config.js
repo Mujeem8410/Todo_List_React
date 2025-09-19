c6d4d40f99bed233c9360401a104c177e51a0e88
Mr.Mujeem <144142191+Mujeem8410@users.noreply.github.com>
2025-09-19 11:26:17 +0530

    Update vite.config.js
    Signed-off-by: Mr.Mujeem <144142191+Mujeem8410@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-\import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/todo-list-react-chi-dusky/', // GitHub repo name or your custom path
